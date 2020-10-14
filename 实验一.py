bbc8ba2afe005c32c31f01e265e228b9488fcef4
lgsq <1798520848@qq.com>
2020-10-14 15:18:58 +0800

    今日实验

---
+++ 实验一.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+num=input('请输入一个自然数')
+print(sum(map(int,num)))
---
+++ 实验二.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+#输入一个自然数，输出他的二进制，八进制，十六进制
+num = int(input('请输入一个自然数: '))
+print('二进制: ', bin(num))
+print('八进制: ', oct(num))
+print('十六进制: ', hex(num))
+#随堂笔记 python中bin代表二进制，oct代表八进制，hex代表十六进制
